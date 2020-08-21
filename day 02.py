b794c2e0ab3125b12676e0bc3f47be1d6f2781fe
bogapooja19 <69467887+bogapooja19@users.noreply.github.com>
2020-08-21 19:10:14 +0530

    Add files via upload

---
+++ day 02.py
@@ -0,0 +1,21 @@
+mport math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tax = meal_cost * (tax_percent/100)
+    tip = meal_cost * (tip_percent/100)
+    total_cost = meal_cost  + tax + tip
+    roundedvalue = round(total_cost)
+    print(roundedvalue)
+if __name__ == '__main__':    
+    meal_cost = float(input())    
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
